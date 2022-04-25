d9acea8018e1827f070aa43c42048e1ff6506f2a
Sam Cooper <sam.cooper@anaplan.com>
2022-04-25 15:49:27 +0100

    update doc comment

--- src/main/kotlin/com/example/Example.kt
+++ src/main/kotlin/com/example/Example.kt
@@ -2,7 +2,9 @@ package com.example
 
 class Example {
     /**
-     * Remove this comment and run detekt again.
+     * This method signature is in the detekt baseline, so it doesn't
+     * cause a failure. But if you remove this comment and run detekt again,
+     * it fails.
      */
     fun checkFoo() {
         println("This method is way too long")
